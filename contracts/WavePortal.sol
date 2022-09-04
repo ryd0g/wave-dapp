7652c65f2c08a0cd35168a9c2174b84c7dc2e564
Ryan <rjlguitar@gmail.com>
2022-09-03 23:47:58 -0700

    created smart contract, test script

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("This is a test smart contract");
+    }
+}
