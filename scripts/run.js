c455efb37e22fecf210d116771be527344687071
Ryan <rjlguitar@gmail.com>
2022-09-03 23:57:50 -0700

    added contract function calls

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,27 @@
 const main = async () => {
+  // contract variables
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
+
+  // log address of contract and deployer
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  // variables from wave contract
+  let count;
+  count = await waveContract.getTotal();
+
+  let txn = await waveContract.wave();
+  await txn.wait();
+
+  count = await waveContract.getTotal();
+
+  txn = await waveContract.connect(randomPerson).wave();
+  await txn.wait();
+
+  count = await waveContract.getTotal();
 };
 
 const runMain = async () => {
