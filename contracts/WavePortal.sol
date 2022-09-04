a41e9928b5af62a75b4376f4a97248b76f8741a0
Ryan <rjlguitar@gmail.com>
2022-09-03 23:57:27 -0700

    added contract functions

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 total;
+
     constructor() {
         console.log("This is a test smart contract");
+    }
+
+    function wave() public {
+        total += 1;
+        console.log("%s waved!", msg.sender);
+    }
+
+    function getTotal() public view returns (uint256) {
+        console.log("%d people have waved!", total);
+        return total;
     }
 }
