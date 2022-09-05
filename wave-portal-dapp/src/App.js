570093f6ba5fc105e3151a353ceb07a76c2b01e4
Ryan <rjlguitar@gmail.com>
2022-09-04 20:34:19 -0700

    added wallet connect

--- wave-portal-dapp/src/App.js
+++ wave-portal-dapp/src/App.js
@@ -1,9 +1,60 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import "./App.css";
 
 export default function App() {
-  const wave = () => {};
+  // state to store user's public wallet
+  const [currentWallet, setCurrentWallet] = useState("");
+  const checkWallet = async () => {
+    try {
+      //make sure browser has access to ethereum window
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Install Metamask and create a wallet!");
+      } else {
+        console.log("Ethereum window connected", ethereum);
+      }
+
+      // check if we can access user wallet
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (!accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found authorized account:", account);
+        setCurrentWallet(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // connect wallet function
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Install Metamask and create a wallet!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentWallet(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // run function on page load
+  useEffect(() => {
+    checkWallet();
+  }, []);
 
   return (
     <div className="mainContainer">
@@ -15,9 +66,18 @@ export default function App() {
           wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave
         </button>
+
+        {/*
+         * If there is no currentAccount render this button
+         */}
+        {!currentWallet && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
